5346e0e855d97a799bee8ad94c30b6ca76d69f5b
SofiaTorres5 <dsofiatorres05@gmail.com>
2026-04-15 17:26:59 -0500

    Refactor file reading and add search function
    Refactored file reading to store all lines in a list and added search functionality.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 archivo = open("videojuegos50registros.csv", "r", encoding="utf-8")
 print("Archivo abierto correctamente\n")
 
-for i in range(5):
-    print(archivo.readline())
+datos = []
 
+for linea in archivo:
+    info = linea.strip().split(",")
+    datos.append(info)
+    
 archivo.close()
+
+#sigue crear las funciones para el menu 
+def buscar(datos, entrada):
+    resultados = []
+    for fila in datos:
+        if entrada.lower() in ",".join(fila).lower():
+            resultados.append(fila)
+    print(f"Se encontraron {len(resultados)} registros")
+    for r in resultados:
+        print(r)
+        
+def estadisticas
+def filtrar
