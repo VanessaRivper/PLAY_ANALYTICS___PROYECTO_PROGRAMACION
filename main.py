161c2daf3667b575ef689bb4a6fefd5add02c048
karolgonzalezb <kagonzalezb@unal.edu.co>
2026-04-15 16:14:21 -0500

    Fix formatting and remove extra spaces in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 try:
     archivo = open("videojuegos50registros.csv", "r", encoding="utf-8")
-    print("Archivo abierto correctamente \n")
+    print("Archivo abierto correctamente\n")
 
     for i in range(5):
         print(archivo.readline())
@@ -8,4 +8,4 @@ try:
     archivo.close()
 
 except:
-    print("Error: no se pudo abrir el archivo ")
+    print("Error: no se pudo abrir el archivo")
