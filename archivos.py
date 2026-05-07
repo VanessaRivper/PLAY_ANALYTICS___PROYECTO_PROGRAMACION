c4ea5190d2d639b50527c3d75808e0b59b97f0b7
SofiaTorres5 <dsofiatorres05@gmail.com>
2026-05-06 19:03:08 -0500

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -6,4 +6,25 @@ def cargar_datos():
   
   datos = []
 
-archivo = open(video
+archivo = open("Video_Games_Sales.csv", "r", endcoding = "utf-8")
+
+lee = csv.reader(archivo)
+
+next(lextor)
+
+for fila in lector:
+  datos.append(fila)
+  
+archivo.close()
+return datos
+
+def guardar_hist(consulta, cantidad):
+  archivo = open("historial.csv", "a", newline = "", encoding = "utf-8")
+
+escritor = csv.writer(archivo)
+
+fecha = datetime. now()
+
+escritor.writerow([fecha, consulta, cantidad])
+
+archivo.close()
