5c3357f27767aeac86012978adffe3ab856bab22
karolgonzalezb <kagonzalezb@unal.edu.co>
2026-05-29 13:10:28 -0500

    Refactor main.py to use PyQt5 for GUI

--- main.py
+++ main.py
@@ -1,50 +1,10 @@
-from archivos import *
-from analisis import *
+import sys
+from PyQt5.QtWidgets import QApplication
+from interfaz import Ventana
 
-datos = cargar_datos()
+app = QApplication(sys.argv)
 
-while True:
-    print("+----------------------------------+")
-    print("|            MENÚ                  |")
-    print("+----------------------------------+")
-    print("|        1. Buscar                 |")
-    print("|        2. Estadísticas           |") 
-    print("|        3. Filtrar                |")
-    print("|        4. Historial              |")
-    print("|        5. Comparar plataformas   |")
-    print("|        6. Salir                  |")
-    print("+----------------------------------+")
-    
-    opcion = input("\n Seleccione una opción: ")
-    
-    if opcion == "1":
-        entrada = input("Ingrese búsqueda: ")
-        resultados = buscar(datos, entrada)
-        guardar_hist(entrada, len(resultados))
-        guardar = input("¿Desea guardar los resultados? (s/n): ")
+ventana = Ventana()
+ventana.show()
 
-        if guardar.lower() == "s":
-            guardar_csv("Busqueda.csv", resultados)
-        
-    elif opcion == "2":
-        estadisticas(datos)
-        guardar_hist("Estadísticas", 1)
-        
-    elif opcion == "3":
-        resultados = filtrar(datos)
-        if resultados:
-            guardar_hist(f"Filtro", len(resultados))
-
-    elif opcion == "4":
-        ver_historial()
-
-    elif opcion == "5":
-        comparar(datos)
-                
-    elif opcion == "6":
-        print("\n¡Adiós! El programa ha finalizado")  
-        break
-    else:
-        print("Opción inválida")
-
-    input("\nPresione Enter para continuar ")
+sys.exit(app.exec_())
